d8bff61cd493e0128b91796ccccb6fbb85415483
Connor Freebairn <ConnorFreebairn@users.noreply.github.com>
2019-05-02 22:29:09 +0100

    Update firstboot.bat

--- firstboot.bat
+++ firstboot.bat
@@ -1,5 +1,5 @@
 set /p name=<hostname.txt
 WMIC computersystem where caption=%computername% rename %name%
-slmgr.vbs /ipk YDG8T-8KH8T-8H8BT-4W8Y7-899PM
+slmgr.vbs /ipk <LIC KEY>
 slmgr.vbs /ato
 shutdown -r -f
