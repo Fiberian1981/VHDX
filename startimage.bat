cfc61450c2db28a2cc4fbe4aaa862f45fb635024
Connor Freebairn <ConnorFreebairn@users.noreply.github.com>
2019-05-04 20:39:12 +0100

    Update startimage.bat

--- startimage.bat
+++ startimage.bat
@@ -1,6 +1,6 @@
 SET name=%computername%
-robocopy \\appsrv2012\c$\OSDEPLOY\NCS.vhdx c:\NCS.vhdx
-diskpart /s \\appsrv2012\c$\OSDEPLOY\attachvhdx.txt
+robocopy \\SRVNAME\c$\OSDEPLOY\NCS.vhdx c:\NCS.vhdx
+diskpart /s \\SRVNAME\c$\OSDEPLOY\attachvhdx.txt
 @echo off
 @echo %name%> d:\hostname.txt
 bcdboot d:\windows
